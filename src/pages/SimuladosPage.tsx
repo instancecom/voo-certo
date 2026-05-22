7551be60ff1e1f8f6ee1d85ab077e7928dc376c8
instancecom <instance.com@gmail.com>
2026-05-22 15:55:40 -0300

    style: hide right-side white gradient fade overlay on mobile

--- src/pages/SimuladosPage.tsx
+++ src/pages/SimuladosPage.tsx
@@ -192,7 +192,7 @@ export default function SimuladosPage() {
                 {/* Fade esquerda */}
                 <div className="pointer-events-none absolute left-0 top-0 bottom-4 w-8 bg-gradient-to-r from-background to-transparent z-10 hidden md:block" />
                 {/* Fade direita */}
-                <div className="pointer-events-none absolute right-0 top-0 bottom-4 w-16 bg-gradient-to-l from-background to-transparent z-10" />
+                <div className="pointer-events-none absolute right-0 top-0 bottom-4 w-16 bg-gradient-to-l from-background to-transparent z-10 hidden md:block" />
 
                 {/* Scroll buttons - Estilo Netflix */}
                 <button 
