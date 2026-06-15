4529df4e6134769677b76ede38299e6ef5d4f4c8
instancecom <instance.com@gmail.com>
2026-06-14 22:19:51 -0300

    fix: travar scroll do background ao abrir preview de insignias

--- src/components/badges/BadgePreviewModal.tsx
+++ src/components/badges/BadgePreviewModal.tsx
@@ -68,11 +68,14 @@ export function BadgePreviewModal({ open, onOpenChange, insignia, earnedAt, appr
   useEffect(() => {
     if (open) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     } else {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     }
     return () => {
       document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     };
   }, [open]);
 
